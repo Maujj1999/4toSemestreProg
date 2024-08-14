58788c31eec403f887744167ee33a23d40f1fc09
Mauro <maurobarroso1999@gmail.com>
2024-08-14 19:40:30 -0300

    Crecion de las carpetas y primera clases de Laboratorio y Metodologia

---
+++ Laboratorio IV/JavaScript/e-commerce/client/js/products.js
@@ -0,0 +1,37 @@
+const productos = [
+    {
+        id: 1,
+        productName: "bebidas",
+        price: 480,
+        quanty: 1,
+        img: "/client/media/bebidas.png"
+    },
+    {
+        id: 2,
+        productName: "carne",
+        price: 950,
+        quanty: 1,
+        img: "/client/media/carne.png"   
+    },
+    {
+        id: 3,
+        productName: "descarga4",
+        price: 400,
+        quanty: 1,
+        img: "/client/media/descarga4.png"
+    },
+    {
+        id: 4,
+        productName: "fruta",
+        price: 700,
+        quanty: 1,
+        img: "/client/media/fruta.png"
+    },
+    {
+        id: 5,
+        productName: "latas",
+        price: 500,
+        quanty: 1,
+        img: "/client/media/latas.png"
+    }
+]
