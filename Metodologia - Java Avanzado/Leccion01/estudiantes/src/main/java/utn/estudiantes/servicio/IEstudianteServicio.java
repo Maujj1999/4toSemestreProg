ae045bf23b4b0295c6027b0b02a591aa0b536bd3
Mauro <maurobarroso1999@gmail.com>
2024-09-11 20:17:47 -0300

    Clase 05 | SpringBoot Parte 4 | Fin Proyecto AppEstudiantes

--- Metodologia - Java Avanzado/Leccion01/estudiantes/src/main/java/utn/estudiantes/servicio/IEstudianteServicio.java
+++ Metodologia - Java Avanzado/Leccion01/estudiantes/src/main/java/utn/estudiantes/servicio/IEstudianteServicio.java
@@ -2,13 +2,13 @@ package utn.estudiantes.servicio;
 
 import java.util.List;
 
-import utn.estudiantes.modelo.Estudiante;
+import utn.estudiantes.modelo.Estudiantes2022;
 
 public interface IEstudianteServicio {
-    public List<Estudiante> listarEstudiantes();
-    public Estudiante buscarEstudiantePorId(Integer idEstudiante);
-    public void guardarEstudiante(Estudiante estudiante);
-    public void eliminarEstudiante(Estudiante estudiante);
+    public List<Estudiantes2022> listarEstudiantes();
+    public Estudiantes2022 buscarEstudiantePorId(Integer idEstudiantes2022);
+    public void guardarEstudiante(Estudiantes2022 estudiante);
+    public void eliminarEstudiante(Estudiantes2022 estudiante);
 
 
 }
