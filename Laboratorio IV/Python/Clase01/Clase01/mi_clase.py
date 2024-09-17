a23c5efddef5267efc0884de8d4b3c116d12b92a
Mauro <maurobarroso1999@gmail.com>
2024-09-16 21:53:43 -0300

    Clase 05 | Profundizando en String

---
+++ Laboratorio IV/Python/Clase01/Clase01/mi_clase.py
@@ -0,0 +1,18 @@
+
+class MiClase:
+    """
+    Este es un ejemplo de la docuemntacion de esta clase.
+    Es solo par saber que aqui esta MiClase.
+    """
+    def __init__(self):
+        """
+        Aqui esta el metodo dunder init
+        Este metodo es el de inicio para esta clase
+        """
+        def mi_metodo(self, param1, param2):
+            """
+            Detecto los parametros automaticamente
+            :param param1: Parametro numero 1
+            :param param2: Parametro numero 2
+            :return: Este es el retorno
+            """
