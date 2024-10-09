ae6b88423869e1d24dc4d7a97cd4e4923fe7bbdd
Mauro <maurobarroso1999@gmail.com>
2024-10-09 16:39:09 -0300

    Clase 08 | Proyecto Tienda Libros con SpringBoot y Swing

--- Metodologia - Java Avanzado/Leccion03/tienda_libros/src/main/java/utn/tienda_libros/vista/LibroFrom.java
+++ Metodologia - Java Avanzado/Leccion03/tienda_libros/src/main/java/utn/tienda_libros/vista/LibroFrom.java
@@ -1,13 +1,20 @@
 package utn.tienda_libros.vista;
 
+import lombok.Builder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import utn.tienda_libros.servicio.LibroServicio;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
 
+@Component
 public class LibroFrom extends JFrame {
     LibroServicio libroServicio;
     private JPanel panel;
+    private JTable tablaLibros;
+    private DefaultTableModel tablaModeloLibros;
 
     @Autowired
     public LibroFrom(LibroServicio libroServicio){
@@ -19,6 +26,40 @@ public class LibroFrom extends JFrame {
         setContentPane(panel);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
-        setSize(900, 700);
+
+        //Para obtener las dimensiones de la ventana
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Dimension tamanioPantalla = toolkit.getScreenSize();
+        int x = (tamanioPantalla.width - getWidth()/2);
+        int y = (tamanioPantalla.height - getWidth()/2);
+        setLocation(x, y);
+    }
+
+    private void createUIComponents() {
+        this.tablaModeloLibros = new DefaultTableModel(0, 5);
+        String[] cabecera = {"id", "Libro", "Autor","Precio", "Existencias"};
+        this.tablaModeloLibros.setColumnIdentifiers(cabecera);
+        //Instanciamos el objeto de JtABLE
+        this.tablaLibros =  new JTable(tablaModeloLibros);
+        listarLibros();
+    }
+
+    private void listarLibros(){
+        //Limpiar tabla:
+        tablaModeloLibros.setRowCount(0);
+        //Obtener los libros BD
+        var libros = libroServicio.listarLibros();
+        //iteramos cada libro:
+        libros.forEach((libro) -> {  //funcion lambda
+            //Creamos cada registro para agregarlos a la tabla:
+            Object [] renglonLibro = {
+                    libro.getIdLibro(),
+                    libro.getNombreLibro(),
+                    libro.getAutor(),
+                    libro.getPrecio(),
+                    libro.getExistencias()
+            };
+            this.tablaModeloLibros.addRow(renglonLibro);
+        });
     }
 }
